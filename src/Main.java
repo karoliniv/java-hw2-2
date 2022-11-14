da38907b25643dbc5c764cfa98404ac8da4dc3b1
karoliniv <46316893+karoliniv@users.noreply.github.com>
2022-11-15 02:13:09 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
         int bonus = 0; //бонус за пополнение
         int giftRubles = 100; //+1 рубль за +100 рублей если >1000 рублей
 
-        mobileAccount = mobileAccount + addMoney;
+        mobileAccount += addMoney;
 
         if (addMoney > 1000) {
             bonus = addMoney / giftRubles;
-            mobileAccount = mobileAccount + bonus;
+            mobileAccount += bonus;
             System.out.println("Бонусов — " + bonus + ", итоговая сумма на счету клиента — "
                     + mobileAccount + " рублей.");
         } else {
